e2d7575864943a677e619813d07e1fff1198f027
ACER@DESKTOP-VEH5UEA <gangadharuppin66@gnail.com>
2025-06-21 11:40:37 +0530

    test

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,12 +1,17 @@
 import pytest
 import logging
 from selenium import webdriver
+import tempfile
+from selenium.webdriver.chrome.options import Options
 
 # Create a session-level fixture to initialize the browser only once
 @pytest.fixture(scope='session')
 def session_driver():
     logging.info('[Session Setup] Launching browser...')
-    driver = webdriver.Chrome()
+    options = Options()
+    user_data_dir = tempfile.mkdtemp()
+    options.add_argument(f'--user-data-dir={user_data_dir}')
+    driver = webdriver.Chrome(options=options)
     yield driver
     logging.info('[Session Teardown] Closing browser...')
     driver.quit()
