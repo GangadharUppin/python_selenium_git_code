7d81ac7a0a3148590ea34e7d871f7c27b6e3272c
ACER@DESKTOP-VEH5UEA <gangadharuppin66@gnail.com>
2025-06-22 12:03:38 +0530

    test based on marker

--- tests/test_amazon.py
+++ tests/test_amazon.py
@@ -8,10 +8,12 @@ from pom.google_page import *
 from utils import Utils
 
 class Test_Amazon:
+    @pytest.mark.sanity
     def test_open_amazon(self, open_amazon):
         self.logging.info("Amazon health check test")
         time.sleep(1)
 
+    @pytest.mark.functional
     def test_amazon_title(self, open_amazon):
         self.logging.info(f"Title of amazon page is{self.driver.title}")
         self.driver.find_element(By.XPATH, search_bar).send_keys("iphone")
--- tests/test_first_basic_tests.py
+++ tests/test_first_basic_tests.py
@@ -6,13 +6,14 @@ from utils import Utils
 
 
 class TestGoogle:
-
+    @pytest.mark.sanity
     def test_google(self):
         self.logging.info("Running test_google")
         self.driver.get("https://www.google.com")
         time.sleep(2)
         self.utils.is_ele_present(google_icon)
 
+    @pytest.mark.functional
     def test_flipkart_health_check(self, open_flipkart):
         self.logging.info("Flipkart health check test")
         time.sleep(6)
