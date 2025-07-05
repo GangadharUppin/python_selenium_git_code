a5a66cca04651d384ac47bf3e5b5ffdd4f6b8f3e
ACER@DESKTOP-VEH5UEA <gangadharuppin66@gnail.com>
2025-07-05 15:24:25 +0530

    testing...

--- tests/test_amazon.py
+++ tests/test_amazon.py
@@ -16,8 +16,10 @@ class Test_Amazon:
 
     @pytest.mark.functional
     def test_amazon_title(self, open_amazon):
-        self.logging.info(f"Title of amazon page is{self.driver.title}")
-        self.driver.find_element(By.XPATH, search_bar).send_keys("iphone")
-        time.sleep(2)
-        self.driver.find_element(By.XPATH, search_icon).click()
-        time.sleep(2)
+        self.logging.info("amazon health check test")
+        time.sleep(6)
+        # self.logging.info(f"Title of amazon page is{self.driver.title}")
+        # self.driver.find_element(By.XPATH, search_bar).send_keys("iphone")
+        # time.sleep(2)
+        # self.driver.find_element(By.XPATH, search_icon).click()
+        # time.sleep(2)
